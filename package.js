28780a210630eeb85b761d0b0d762379c2a23219
Valentin Funk <funk.valentin@gmail.com>
2015-12-03 17:21:28 +0000

    Use fixed version

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-    name: "kidovate:accounts-steam",
+    name: "kamshak:accounts-steam",
     summary: "Login service for Steam.",
     version: '1.0.1',
-    git: 'https://github.com/paralin/accounts-steam.git',
+    git: 'https://github.com/kamshak/accounts-steam.git',
     documentation: null
 });
 
@@ -14,7 +14,7 @@ Package.on_use(function(api) {
     // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
     api.use('accounts-oauth@1.1.2', ['client', 'server']);
-    api.use('kidovate:steam@1.0.1', ['client', 'server']);
+    api.use('kamshak:steam@1.0.1', ['client', 'server']);
 
     api.add_files('steam_login_button.css', 'client');
 
