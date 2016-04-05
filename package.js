618a039550b4cd7abbdca016273fb77eb82ac69d
Valentin Funk <funk.valentin@gmail.com>
2016-04-05 22:45:58 +0200

    version bump

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
     name: "kamshak:accounts-steam",
     summary: "Login service for Steam.",
-    version: '1.0.1',
+    version: '1.0.2',
     git: 'https://github.com/kamshak/accounts-steam.git',
     documentation: null
 });
 
 Package.on_use(function(api) {
-    api.versionsFrom('1.0');
+    api.versionsFrom('1.3');
 
-    api.use(['underscore@1.0.1', 'random@1.0.1']);
-    api.use('accounts-base@1.1.2', ['client', 'server']);
+    api.use(['underscore', 'random']);
+    api.use('accounts-base', ['client', 'server']);
     // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
-    api.use('accounts-oauth@1.1.2', ['client', 'server']);
+    api.use('accounts-oauth', ['client', 'server']);
     api.use('kamshak:steam@1.0.1', ['client', 'server']);
 
     api.add_files('steam_login_button.css', 'client');
